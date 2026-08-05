09821a09870c42c2047db183770f8d855640dd32
tvvpeacock <saluuuthkaa@outlook.com>
2026-08-05 13:02:51 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,21 @@
+
+project = 'Peacock TV'
+author =  'Peacock TV'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
